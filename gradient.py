6729be41487cb1dcd4ed55ddd2068194a1f7741e
Jonathan Soo <jcsoo@agora.com>
2018-01-04 15:41:14 -0500

    Use HSV colorspace and ALL

--- gradient.py
+++ gradient.py
@@ -10,12 +10,12 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block, vis=False, featu
     if vis == True:
         features, hog_image = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
                                   cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=False, 
-                                  visualise=True, feature_vector=False)
+                                  visualise=True, feature_vector=False, block_norm="L2-Hys")
         return features, hog_image
     else:      
         features = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
                        cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=False, 
-                       visualise=False, feature_vector=feature_vec)
+                       visualise=False, feature_vector=feature_vec, block_norm="L2-Hys")
         return features
 
 def main(args):
