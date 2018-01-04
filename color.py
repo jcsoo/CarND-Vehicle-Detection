68f21d9b065018043baffad5780f40b28699a7eb
Jonathan Soo <jcsoo@agora.com>
2018-01-04 11:39:16 -0500

    Add colorspace and HOG

--- color.py
+++ color.py
@@ -6,8 +6,28 @@ from loader import *
 
 COLOR_SPACES = ['RGB', 'HSV', 'LUV', 'HLS', 'YUV', 'YCrCb']
 
+def to_colorspace(img, color_space='RGB'):
+    # Convert image to new color space (if specified)
+    if color_space != 'RGB':
+        if color_space == 'HSV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        elif color_space == 'LUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
+        elif color_space == 'HLS':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+        elif color_space == 'YUV':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+        elif color_space == 'YCrCb':
+            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
+        else:
+            raise Exception('Unknown colorspace %s' % color_space)
+    else: 
+        feature_image = np.copy(img)         
+    return feature_image    
+
 # Define a function to compute color histogram features  
-def rgb_hist(img, nbins=32, bins_range=(0, 256)):
+def rgb_hist(img, color_space='RGB', nbins=32, bins_range=(0, 256)):
+    img = to_colorspace(img, color_space)
     # Compute the histogram of the RGB channels separately
     rhist = np.histogram(img[:,:,0], bins=nbins, range=bins_range)
     ghist = np.histogram(img[:,:,1], bins=nbins, range=bins_range)
@@ -22,30 +42,18 @@ def rgb_hist(img, nbins=32, bins_range=(0, 256)):
     
 # Generate colorspace histogram vector
 def bin_spatial(img, color_space='RGB', size=(32, 32)):
-    # Convert image to new color space (if specified)
-    if color_space != 'RGB':
-        if color_space == 'HSV':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-        elif color_space == 'LUV':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
-        elif color_space == 'HLS':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-        elif color_space == 'YUV':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-        elif color_space == 'YCrCb':
-            feature_image = cv2.cvtColor(img, cv2.COLOR_RGB2YCrCb)
-    else: feature_image = np.copy(img)             
+    feature_image = to_colorspace(img, color_space)
     # Use cv2.resize().ravel() to create the feature vector
     features = cv2.resize(feature_image, size).ravel() 
     # Return the feature vector
     return features
 
-def plot_rgb_histogram(item):
+def plot_histogram(item, color_space='RGB'):
     path = item['path']
     tag = item['tag']
     img = load_image(path)
 
-    rhist, ghist, bhist, bin_centers, hist_features = rgb_hist(img)
+    rhist, ghist, bhist, bin_centers, hist_features = rgb_hist(img, color_space)
 
     # Plot a figure with all three bar charts
     fig = plt.figure(figsize=(12,3))        
@@ -55,15 +63,15 @@ def plot_rgb_histogram(item):
     plt.subplot(142)
     plt.bar(bin_centers, rhist[0])
     plt.xlim(0, 256)
-    plt.title('R Histogram')
+    plt.title('%s Histogram' % color_space[0])
     plt.subplot(143)
     plt.bar(bin_centers, ghist[0])
     plt.xlim(0, 256)
-    plt.title('G Histogram')
+    plt.title('%s Histogram' % color_space[1])
     plt.subplot(144)
     plt.bar(bin_centers, bhist[0])
     plt.xlim(0, 256)
-    plt.title('B Histogram')
+    plt.title('%s Histogram' % color_space[2])
     plt.show()
 
 def plot_bin_spatial(item, color_space='RGB'):
@@ -88,7 +96,8 @@ def plot_bin_spatial(item, color_space='RGB'):
 
 def main(args):
     for item in samples(5):
-        plot_bin_spatial(item)
+        # plot_bin_spatial(item)
+        plot_histogram(item, args[0])
 
 
 if __name__ == '__main__':
---
+++ gradient.py
@@ -0,0 +1,49 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+from skimage.feature import hog
+from loader import *
+
+# Define a function to return HOG features and visualization
+def get_hog_features(img, orient, pix_per_cell, cell_per_block, vis=False, feature_vec=True):
+    if vis == True:
+        features, hog_image = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+                                  cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=False, 
+                                  visualise=True, feature_vector=False, block_norm="L2-Hys")
+        return features, hog_image
+    else:      
+        features = hog(img, orientations=orient, pixels_per_cell=(pix_per_cell, pix_per_cell),
+                       cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=False, 
+                       visualise=False, feature_vector=feature_vec, block_norm="L2-Hys")
+        return features
+
+def main(args):
+    for item in samples(5):
+        path, tag = item['path'], item['tag']
+        img = load_image(path)[:,:,0]
+
+
+        features, hog_image = get_hog_features(img, 9, 8, 2, True, True)
+
+        
+        fig = plt.figure(figsize=(12,3))
+        plt.subplot(131)
+        plt.imshow(img)
+        plt.title('%s' % tag)    
+
+        plt.subplot(132)
+        plt.imshow(hog_image)
+        plt.title('features')
+
+        plt.subplot(133)
+        plt.plot(features.ravel())
+        
+
+        plt.show()
+        
+
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
