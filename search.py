34230691ed0ab0aa217695a8352b603eb3ad1ef1
Jonathan Soo <jcsoo@agora.com>
2018-01-05 10:07:28 -0500

    Add heatmap and thresholding

--- search.py
+++ search.py
@@ -11,6 +11,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
 from skimage.feature import hog
 from sklearn.model_selection import train_test_split, GridSearchCV
+from scipy.ndimage.measurements import label
 
 def load_spec(spec_file):
     with open(spec_file) as f:
@@ -178,6 +179,37 @@ def find_cars(img, y_start_stop, scale, clf, scl, spec):
                 
     return boxes
 
+def add_heat(heatmap, bbox_list):
+    # Iterate through list of bboxes
+    for box in bbox_list:
+        # Add += 1 for all pixels inside each bbox
+        # Assuming each "box" takes the form ((x1, y1), (x2, y2))
+        heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
+
+    # Return updated heatmap
+    return heatmap# Iterate through list of bboxes
+
+def apply_threshold(heatmap, threshold):
+    # Zero out pixels below the threshold
+    heatmap[heatmap <= threshold] = 0
+    # Return thresholded map
+    return heatmap
+
+def draw_labeled_bboxes(img, labels):
+    # Iterate through all detected cars
+    for car_number in range(1, labels[1]+1):
+        # Find pixels with each car_number label value
+        nonzero = (labels[0] == car_number).nonzero()
+        # Identify x and y values of those pixels
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Define a bounding box based on min/max x and y
+        bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
+        # Draw the box on the image
+        cv2.rectangle(img, bbox[0], bbox[1], (0,0,255), 6)
+    # Return the image
+    return img
+
 
 def search(spec_file, paths):
     spec, clf, scl = load_spec(spec_file)
@@ -186,7 +218,7 @@ def search(spec_file, paths):
     y_start_stop = [400, 700] # Min and max in y to search in slide_window()    
     sizes = [128, 96, 64]
     # scales = [1.0, 1.5, 2.0]
-    scales = [1.5, 2.0]
+    scales = [1.0, 1.5, 2.0]
 
     for path in paths:
         img = load_image(path)
@@ -203,11 +235,32 @@ def search(spec_file, paths):
                 windows = slide_window(img.shape, x_start_stop=[None, None], y_start_stop=y_start_stop, xy_window=(size, size), xy_overlap=(0.5, 0.5))
                 hot_windows.extend(search_windows(feature_img, windows, clf, scl, spec))
 
-        draw_img = draw_boxes(draw_img, hot_windows, color=(0, 0, 255), thick=6)                
+        # draw_img = draw_boxes(draw_img, hot_windows, color=(0, 0, 255), thick=6)          
+        # plt.imshow(draw_img)
+        # plt.show()        
+
+        heat = np.zeros_like(img[:,:,0]).astype(np.float)
+        add_heat(heat, hot_windows)
+    
+        # Apply threshold to help remove false positives
+        heat = apply_threshold(heat, 3)
+
+        # Visualize the heatmap when displaying    
+        heatmap = np.clip(heat, 0, 255)    
+        # Find final boxes from heatmap using label function
+        labels = label(heatmap)
+
+        draw_img = draw_labeled_bboxes(draw_img, labels)        
+
+        fig = plt.figure()
+        plt.subplot(121)
         plt.imshow(draw_img)
-        plt.show()        
-
-
+        plt.title('Car Positions')
+        plt.subplot(122)
+        plt.imshow(heatmap, cmap='hot')
+        plt.title('Heat Map')
+        fig.tight_layout()
+        plt.show()
 
 
 
